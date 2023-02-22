58ddbbd3d801c7742040a3d5da949ccd8edb43fd
Yury <yurafikys@gmail.com>
2023-02-22 22:01:20 +0300

    Исправил замечания

--- 4-dummy-api/req.ts
+++ 4-dummy-api/req.ts
@@ -1,81 +1,90 @@
 const axios = require('axios').default;
+const url: string = 'https://dummyjson.com/users';
 
-type Gender = 'male' | 'female'
+enum Gender {
+  male = 'male',
+  female = 'female'
+}
+
 
 interface Hair {
-  color: string
-  type: string
+  color: string;
+  type: string;
 }
 
 interface Coordinates {
-  lat: number
-  lng: number
+  lat: number;
+  lng: number;
 }
 
 interface Address {
-  address: string
-  city: string
-  coordinates: Coordinates
-  postalCode: string
-  state: string
+  address: string;
+  city: string;
+  coordinates: Coordinates;
+  postalCode: string;
+  state: string;
 }
 
 interface Bank {
-  cardExpire: string
-  cardNumber: string
-  cardType: string
-  currency: string
-  iban: string
+  cardExpire: string;
+  cardNumber: string;
+  cardType: string;
+  currency: string;
+  iban: string;
 }
 
 interface Company {
-  address: Address
-  department: string
-  name: string
-  title: string
+  address: Address;
+  department: string;
+  name: string;
+  title: string;
 }
 
 interface Response {
-  id: number
-  firstName: string
-  lastName: string
-  maidenName: string
-  age: number
-  gender: Gender
-  email: string
-  phone: string
-  username: string
-  password: string
-  birthDate: string
-  image: string
-  bloodGroup: string
-  height: number
-  weight: number
-  eyeColor: string
-  hair: Hair
-  domain: string
-  ip: string
-  address: Address
-  macAddress: string
-  university: string
-  bank: Bank
-  company: Company
-  ein: string
-  ssn: string
-  userAgent: string
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenName: string;
+  age: number;
+  gender: Gender;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: Hair;
+  domain: string;
+  ip: string;
+  address: Address;
+  macAddress: string;
+  university: string;
+  bank: Bank;
+  company: Company;
+  ein: string;
+  ssn: string;
+  userAgent: string;
 }
 
-axios.get('https://dummyjson.com/users')
-  .then(function (response: unknown) {
-    if (response instanceof Response) {
-      console.log(response);
-    }
-  })
-  .catch(function (error: unknown) {
+async function getUser(url: string) {
+  try {
+    await axios.get(url)
+      .then(function (response: Response) {
+        console.log(response);
+      })
+  } catch (error: unknown) {
     if (error instanceof Error) {
       console.log(error.message);
     }
-  });
+  };
+}
+
+
+
 
 
 
